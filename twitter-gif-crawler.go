bbb9d82dd9270a1eb14565bcc8fefb4a25afe41f
Fabien Foerster <fabienfoerster@gmail.com>
2015-11-30 21:26:50 +0100

    don't go boum when no gif in tweet

--- twitter-gif-crawler.go
+++ twitter-gif-crawler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/url"
 
@@ -10,9 +9,9 @@ import (
 )
 
 func main() {
+
 	//set up firebase connection
 	f := firego.New("https://gif-wall.firebaseio.com/gif_list")
-
 	//set up twitter api
 	anaconda.SetConsumerKey("2vxn8zOFZuBPCep3oejC33SsX")
 	anaconda.SetConsumerSecret("8mjb3gIK7agiZYvEiQQHFjxDbqMMzqCzrbNXJg0aDkTyHt3NIk")
@@ -20,23 +19,33 @@ func main() {
 	v := url.Values{}
 	v.Set("follow", "168749152")
 	stream := api.PublicStreamFilter(v)
-	fmt.Println("Coucou c'est le début")
+	log.Println("coucou")
 	for t := range stream.C {
 		var tweet = t.(anaconda.Tweet)
 		// need a cleaner way to do that
-		url := tweet.ExtendedEntities.Media[0].VideoInfo.Variants[0].Url
-		fmt.Println(url)
+		if url := extractGifFromMediaTwitter(tweet); url != "" {
+			pushedFirego, err := f.Push(url)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		pushedFirego, err := f.Push(url)
-		if err != nil {
-			log.Fatal(err)
+			var bar string
+			if err := pushedFirego.Value(&bar); err != nil {
+				log.Fatal(err)
+			}
 		}
-
-		var bar string
-		if err := pushedFirego.Value(&bar); err != nil {
-			log.Fatal(err)
-		}
-
 	}
+
+}
+
+func extractGifFromMediaTwitter(tweet anaconda.Tweet) string {
+
+	if medias := tweet.ExtendedEntities.Media; len(medias) != 0 {
+		if variants := medias[0].VideoInfo.Variants; len(variants) != 0 {
+			url := variants[0].Url
+			return url
+		}
+	}
+	return ""
 
 }
