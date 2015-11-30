56ed62049c66043f623c1a8fb155ac7018ab2572
Fabien Foerster <fabienfoerster@gmail.com>
2015-11-30 21:50:35 +0100

    log a little bit useful ( but just a little )

--- twitter-gif-crawler.go
+++ twitter-gif-crawler.go
@@ -12,17 +12,19 @@ func main() {
 
 	//set up firebase connection
 	f := firego.New("https://gif-wall.firebaseio.com/gif_list")
+	log.Println("Connected to Firebase ...")
 	//set up twitter api
 	anaconda.SetConsumerKey("2vxn8zOFZuBPCep3oejC33SsX")
 	anaconda.SetConsumerSecret("8mjb3gIK7agiZYvEiQQHFjxDbqMMzqCzrbNXJg0aDkTyHt3NIk")
 	api := anaconda.NewTwitterApi("1620076356-iQfWV1al7DCa3XVbfl3mLsmAS2GgCEB22WFtGgu", "lbhjpDSZ7Qx0fcG75P9YMC7sLB0UTlnSF8OAminwOyRZU")
+	log.Println("Connected to Twitter Stream API ....")
 	v := url.Values{}
 	v.Set("follow", "168749152")
 	stream := api.PublicStreamFilter(v)
-	log.Println("coucou")
+	log.Println("Stream initialized ....")
 	for t := range stream.C {
-		var tweet = t.(anaconda.Tweet)
-		// need a cleaner way to do that
+		tweet := t.(anaconda.Tweet)
+		log.Printf("New tweet : https://twitter.com/%s/status/%s", tweet.User.ScreenName, tweet.IdStr)
 		if url := extractGifFromMediaTwitter(tweet); url != "" {
 			pushedFirego, err := f.Push(url)
 			if err != nil {
@@ -43,9 +45,11 @@ func extractGifFromMediaTwitter(tweet anaconda.Tweet) string {
 	if medias := tweet.ExtendedEntities.Media; len(medias) != 0 {
 		if variants := medias[0].VideoInfo.Variants; len(variants) != 0 {
 			url := variants[0].Url
+			log.Printf("Media GIF found : %s", url)
 			return url
 		}
 	}
+	log.Println("No media GIF found")
 	return ""
 
 }
