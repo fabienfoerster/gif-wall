37115445b929154e0615703b31586ff4656ac5f1
Fabien Foerster <fabienfoerster@gmail.com>
2015-11-30 23:09:54 +0100

    refactor #7 begins

--- gif-wall.go
+++ gif-wall.go
@@ -1,9 +1,8 @@
-package main
+package gifwall
 
 import (
 	"log"
 	"net/url"
-	"path/filepath"
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/CloudCom/firego"
@@ -50,33 +49,4 @@ func saveToFirese(f *firego.Firebase, url string) {
 		log.Fatal(err)
 	}
 	log.Println("Save to Firebase successful !")
-}
-
-func extractGIFFromMediaTwitter(tweet anaconda.Tweet) string {
-
-	if medias := tweet.ExtendedEntities.Media; len(medias) != 0 {
-		if variants := medias[0].VideoInfo.Variants; len(variants) != 0 {
-			url := variants[0].Url
-			log.Printf("Media GIF found : %s", url)
-			return url
-		}
-	}
-	log.Println("No media GIF found")
-	return ""
-
-}
-
-func extractGIFFromLinkTwitter(tweet anaconda.Tweet) string {
-	if urls := tweet.Entities.Urls; len(urls) != 0 {
-		for _, url := range urls {
-			expendedURL := url.Expanded_url
-			if extension := filepath.Ext(expendedURL); extension == ".gif" {
-				log.Printf("GIF link found : %s", expendedURL)
-				return expendedURL
-			}
-		}
-
-	}
-	log.Println("No GIF link found")
-	return ""
 }
---
+++ twitter-crawler.go
@@ -0,0 +1,37 @@
+package gifwall
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/ChimeraCoder/anaconda"
+)
+
+func extractGIFFromMediaTwitter(tweet anaconda.Tweet) string {
+
+	if medias := tweet.ExtendedEntities.Media; len(medias) != 0 {
+		if variants := medias[0].VideoInfo.Variants; len(variants) != 0 {
+			url := variants[0].Url
+			log.Printf("Media GIF found : %s", url)
+			return url
+		}
+	}
+	log.Println("No media GIF found")
+	return ""
+
+}
+
+func extractGIFFromLinkTwitter(tweet anaconda.Tweet) string {
+	if urls := tweet.Entities.Urls; len(urls) != 0 {
+		for _, url := range urls {
+			expendedURL := url.Expanded_url
+			if extension := filepath.Ext(expendedURL); extension == ".gif" {
+				log.Printf("GIF link found : %s", expendedURL)
+				return expendedURL
+			}
+		}
+
+	}
+	log.Println("No GIF link found")
+	return ""
+}
